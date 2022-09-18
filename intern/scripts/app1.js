275f0512095ed4c8fcc9c60d320bb5a9d4ec51be
krrishexe <aryan.yadav.9889@gmail.com>
2022-09-19 00:18:53 +0530

    updated label and button tag

--- intern/scripts/app1.js
+++ intern/scripts/app1.js
@@ -1,5 +1,5 @@
 
- $("#div2").hide();
+$("#div2").hide();
 
 "use strict";
 var zip = new JSZip();
@@ -7,21 +7,21 @@ var files = []
 var test = []
 var contents = []
 var aa = []
-var singleFile 
+var singleFile
 var urls = []
 
 // for reading files
 
-$("#file").on("change", function(evt) {
+$("#file").on("change", function (evt) {
     function handleFile(f) {
 
-        JSZip.loadAsync(f)                                  
-            .then(function(zip) {
+        JSZip.loadAsync(f)
+            .then(function (zip) {
 
                 singleFile = zip.files;
 
-                zip.forEach(  function (relativePath, zipEntry) { 
-                    
+                zip.forEach(function (relativePath, zipEntry) {
+
                     var y = zipEntry.name
                     test.push(y);
                     urls.push(zipEntry);
@@ -30,11 +30,11 @@ $("#file").on("change", function(evt) {
                 });
             }, function (e) {
                 $result.append($("<div>", {
-                    "class" : "alert alert-danger",
-                    text : "Error reading " + f.name + ": " + e.message
+                    "class": "alert alert-danger",
+                    text: "Error reading " + f.name + ": " + e.message
                 }));
             });
-            
+
     }
 
     var files = evt.target.files;
@@ -43,24 +43,24 @@ $("#file").on("change", function(evt) {
     }
 
     setTimeout(function () {
-      tree()
+        tree()
     }, 1300);
 });
 
 // to create jstree view
 
-function tree(){
+function tree() {
 
-    aa = test.sort((a,b) => a.length - b.length);
+    aa = test.sort((a, b) => a.length - b.length);
 
     contents.push(
-        {"id": `${aa[0]}`, "parent": "#", "text": `${aa[0]}`, "a_attr": {"href": "javascript.submitMe()"}}
+        { "id": `${aa[0]}`, "parent": "#", "text": `${aa[0]}`, "a_attr": { "href": "javascript.submitMe()" } }
     )
 
-    for(let x = 1; x<=(aa.length)-1; x++){
+    for (let x = 1; x <= (aa.length) - 1; x++) {
 
-     contents.push({"id": `${aa[x]}`, "parent": `${compare(x)}`, "text": `${compare1(x)}`,"a_attr": {"href": "javascript.submitMe()"}},)
-    
+        contents.push({ "id": `${aa[x]}`, "parent": `${compare(x)}`, "text": `${compare1(x)}`, "a_attr": { "href": "javascript.submitMe()" } },)
+
     }
     createTree();
 
@@ -74,67 +74,67 @@ function tree(){
     }, 1000);
 }
 
-function compare(x){
+function compare(x) {
 
-    if(aa[x].localeCompare(aa[x].match(/(.*\/)/g))){
+    if (aa[x].localeCompare(aa[x].match(/(.*\/)/g))) {
         return aa[x].match(/(.*\/)/g)
-    }else{
-        return  aa[x].match(/.+(?=.*\/.*\/)\//g)
+    } else {
+        return aa[x].match(/.+(?=.*\/.*\/)\//g)
     }
 
 }
 
-function compare1(x){
+function compare1(x) {
 
-    if(aa[x].match(/[^/]*$/g)== ''){
-        return  aa[x].match(/[^\/]*(?=\/[^\/]*$)/g)
-    }else{
+    if (aa[x].match(/[^/]*$/g) == '') {
+        return aa[x].match(/[^\/]*(?=\/[^\/]*$)/g)
+    } else {
         return aa[x].match(/[^/]*$/g)
     }
 
 }
 
-function createTree(){
+function createTree() {
     createJSTree(contents);
-    
+
 }
 
-function createJSTree(contents) {          
+function createJSTree(contents) {
     $('#tree').jstree({
         'core': {
             'data': contents
         },
-        "plugins" : [ "themes", "html_data", "sort", "ui" ]
+        "plugins": ["themes", "html_data", "sort", "ui"]
     });
-    
+
 }
 
 // to unzip and download
 
-function submitMe(){ 
+function submitMe() {
     var result = $('#tree').jstree('get_selected');
-        console.log('result',result)
+    console.log('result', result)
 
-    for(let x = 0;x<result.length;x++){
-    
-        var  name = singleFile[`${result[x]}`]
+    for (let x = 0; x < result.length; x++) {
+
+        var name = singleFile[`${result[x]}`]
         files.push(name)
-    
+
     }
-           
-        for (var i = 0; i < files.length; i++) {
-            var f = files[i];
-                    zip.file(f.name, f._data);
-                 }
-                 zip.generateAsync({type:"blob"}).then(function (blob) {
-                    saveAs(blob, `${f.name}`)                          
-                })
 
-} 
+    for (var i = 0; i < files.length; i++) {
+        var f = files[i];
+        zip.file(f.name, f._data);
+    }
+    zip.generateAsync({ type: "blob" }).then(function (blob) {
+        saveAs(blob, `${f.name}`)
+    })
 
-function clicked(){
-    
-    $('a').click(function(){
+}
+
+function clicked() {
+
+    $('a').click(function () {
         console.log('clicked')
         setTimeout(function () {
             console.log('submit')
@@ -145,25 +145,25 @@ function clicked(){
 }
 
 
-function downloadAll(){
+function downloadAll() {
     var bb = []
     console.log(urls)
-    for(let x = 0;x<urls.length;x++){
+    for (let x = 0; x < urls.length; x++) {
 
         bb.push(urls[x]._data)
 
     }
 
-    for(let x = 0; x<bb.length;x++){
+    for (let x = 0; x < bb.length; x++) {
 
         zip.file(urls[x].name, bb[x]);
-        zip.generateAsync({type:"blob"}).then(function (blob) {
-            saveAs(blob, `${urls[x].name}`)                          
+        zip.generateAsync({ type: "blob" }).then(function (blob) {
+            saveAs(blob, `${urls[x].name}`)
         })
-   
-        }
-        }
 
-    function refresh(){
-        location.reload();
     }
+}
+
+// function refresh() {
+//     location.reload();
+// }
